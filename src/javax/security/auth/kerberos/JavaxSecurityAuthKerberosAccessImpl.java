474e15dfeff75067bdf538da0453773f36b20a3c
dengjianping888 <hand-488@crc.com.hk>
2020-12-03 19:14:41 +0800

    [问题单号]Version1.0.0 [问题描述]ArrayList源码分析

---
+++ src/javax/security/auth/kerberos/JavaxSecurityAuthKerberosAccessImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package javax.security.auth.kerberos;
+
+import sun.security.krb5.JavaxSecurityAuthKerberosAccess;
+import sun.security.krb5.EncryptionKey;
+import sun.security.krb5.PrincipalName;
+
+class JavaxSecurityAuthKerberosAccessImpl implements JavaxSecurityAuthKerberosAccess {
+    @Override
+    public sun.security.krb5.internal.ktab.KeyTab keyTabTakeSnapshot(KeyTab ktab) {
+        return ktab.takeSnapshot();
+    }
+
+    @Override
+    public KerberosTicket kerberosTicketGetProxy(KerberosTicket kerberosTicket) {
+        return kerberosTicket.proxy;
+    }
+
+    @Override
+    public void kerberosTicketSetProxy(KerberosTicket kerberosTicket, KerberosTicket kerberosTicket1) {
+        kerberosTicket.proxy = kerberosTicket1;
+    }
+}
