b0398b3fb9d977228addbd53e9c5443772add516
dengjianping888 <hand-488@crc.com.hk>
2020-12-04 10:47:35 +0800

    [问题单号]Version1.0.0 [问题描述]ArrayList源码分析

---
+++ src/java/util/jar/JavaUtilJarAccessImpl.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package java.util.jar;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.Enumeration;
+import java.util.List;
+import sun.misc.JavaUtilJarAccess;
+
+class JavaUtilJarAccessImpl implements JavaUtilJarAccess {
+    @Override
+    public boolean jarFileHasClassPathAttribute(JarFile jar) throws IOException {
+        return jar.hasClassPathAttribute();
+    }
+
+    @Override
+    public CodeSource[] getCodeSources(JarFile jar, URL url) {
+        return jar.getCodeSources(url);
+    }
+    @Override
+    public CodeSource getCodeSource(JarFile jar, URL url, String name) {
+        return jar.getCodeSource(url, name);
+    }
+    @Override
+    public Enumeration<String> entryNames(JarFile jar, CodeSource[] cs) {
+        return jar.entryNames(cs);
+    }
+    @Override
+    public Enumeration<JarEntry> entries2(JarFile jar) {
+        return jar.entries2();
+    }
+    @Override
+    public void setEagerValidation(JarFile jar, boolean eager) {
+        jar.setEagerValidation(eager);
+    }
+    @Override
+    public List<Object> getManifestDigests(JarFile jar) {
+        return jar.getManifestDigests();
+    }
+
+    @Override
+    public Attributes getTrustedAttributes(Manifest man, String name) {
+        return man.getTrustedAttributes(name);
+    }
+
+    @Override
+    public void ensureInitialization(JarFile jarFile) {
+        jarFile.ensureInitialization();
+    }
+}
